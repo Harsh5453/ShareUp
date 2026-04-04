7539dbc843a479d9f5426550d72c13fe6f162a13
Harsh <h664363@gmail.com>
2026-04-04 13:25:51 +0530

    Add Future validation to endDate in BorrowRequestDTO

--- rental-service/src/main/java/com/shareup/rental/dto/BorrowRequestDTO.java
+++ rental-service/src/main/java/com/shareup/rental/dto/BorrowRequestDTO.java
@@ -1,8 +1,10 @@
 package com.shareup.rental.dto;
 
 import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Future;   
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
