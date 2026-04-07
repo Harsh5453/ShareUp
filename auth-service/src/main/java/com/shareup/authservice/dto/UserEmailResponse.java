4ab666a6bdcc02437f62c5302c141f15c264dc1e
Harsh <h664363@gmail.com>
2026-04-07 21:37:03 +0530

    Add address field to UserEmailResponse DTO

--- auth-service/src/main/java/com/shareup/authservice/dto/UserEmailResponse.java
+++ auth-service/src/main/java/com/shareup/authservice/dto/UserEmailResponse.java
@@ -5,7 +5,8 @@ public class UserEmailResponse {
     private Long id;
     private String email;
     private String name;
-    private String phone;  
+    private String phone;
+    private String address;  
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -18,4 +19,7 @@ public class UserEmailResponse {
 
     public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
 }
